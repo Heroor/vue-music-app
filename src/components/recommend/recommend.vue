a4a0d3f157c3cfef59559fdf34a649e3fa09a723
fbj <fbj@cto100.com>
2017-12-04 17:20:15 +0800

    update recommend

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
-    recommend
+    <ul>
+      <li v-for="(item, index) in slider" :key="index">
+        {{item.id}}
+      </li>
+    </ul>
   </div>
 </template>
-
 <script>
-  import {
-    getRecommend
-  } from 'api/recommend'
-  export default {
-    created () {
-      getRecommend().then(res => {
-        console.log(res)
-      })
+import {
+  getRecommend
+} from 'api/recommend'
+import { ERR_OK } from 'api/config'
+export default {
+  data () {
+    return {
+      songList: [],
+      slider: []
     }
+  },
+  created () {
+    getRecommend().then(res => {
+      if (res.code === ERR_OK) {
+        console.log(res)
+        const data = res.data
+        this.slider = data.slider
+      }
+    })
   }
+}
 
 </script>
-
 <style>
 
 
